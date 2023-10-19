948097e2803a54b68d687b72415dc4508c73e86a
moeen775 <moeenahmmed2015br@gmail.com>
2023-10-20 00:16:34 +0600

    project setup

---
+++ src/test/java/com/moeen/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moeen.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
