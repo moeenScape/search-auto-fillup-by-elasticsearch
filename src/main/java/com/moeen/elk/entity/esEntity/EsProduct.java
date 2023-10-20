e5496991cb3a10486e8343eabec794bba4ed1e1c
moeen775 <moeenahmmed2015br@gmail.com>
2023-10-20 15:14:26 +0600

    implement elasticsearch Auto suggestion

--- src/main/java/com/moeen/elk/entity/esEntity/EsProduct.java
+++ src/main/java/com/moeen/elk/entity/esEntity/EsProduct.java
@@ -16,10 +16,9 @@ public class EsProduct {
     private String type;
     private int qty;
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getName() {
+        return name;
     }
-
 
     public void setName(String name) {
         this.name = name;
